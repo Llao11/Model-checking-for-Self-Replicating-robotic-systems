0fb8f5d277b256bb7426ff4bb0ea1d27da2886a1
Dmitry Babaytsev <dmitry.babaytsev@cgi.com>
2025-03-02 19:09:34 +0100

    integrate ros2-gz bridge for detachable joint to launch file

--- ros2_ws/src/srrs_sim/launch/robot.launch.py
+++ ros2_ws/src/srrs_sim/launch/robot.launch.py
@@ -43,32 +43,12 @@ def generate_launch_description():
         ]
     )
 
-
-    # # ROS-Ign bridge for service communication
-    # Node(
-    #     package='ros_ign_bridge',
-    #     executable='parameter_bridge',
-    #     name='ignition_bridge',
-    #     output='screen',
-    #     parameters=[{
-    #         'config': """
-    #         [
-    #             {
-    #                 'ros_service_name': '/world/create_entity',
-    #                 'ign_service_name': '/world/default/create',
-    #                 'ros_service_type': 'ros_ign_interfaces/srv/SpawnEntity',
-    #                 'ign_service_type': 'ignition.msgs.EntityFactory'
-    #             },
-    #             {
-    #                 'ros_service_name': '/world/remove_entity',
-    #                 'ign_service_name': '/world/default/remove',
-    #                 'ros_service_type': 'ros_ign_interfaces/srv/DeleteEntity',
-    #                 'ign_service_type': 'ignition.msgs.Entity'
-    #             }
-    #         ]
-    #         """
-    #     }]
-    # ),
+    bridge_node = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{'config_file': bridge_config_path}],
+        output='screen'
+    )
 
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -212,6 +192,7 @@ def generate_launch_description():
         # gz_spawn_base,
         gz_spawn_parts,
         gz_spawn_robot,
+        bridge_node, # for attaching and detaching joints
         
         # Launch Arguments
         DeclareLaunchArgument(
