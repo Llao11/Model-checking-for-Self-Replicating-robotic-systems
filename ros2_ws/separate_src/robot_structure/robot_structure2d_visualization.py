562a32b351fc9b7efb2fc16ef98721faf5042259
Dmitry Babaytsev <dmitry.babaytsev@cgi.com>
2025-07-20 23:19:53 +0200

    changes in smv and in visualization tool

--- ros2_ws/separate_src/robot_structure/robot_structure2d_visualization.py
+++ ros2_ws/separate_src/robot_structure/robot_structure2d_visualization.py
@@ -1,23 +1,13 @@
 import matplotlib.pyplot as plt
 from typing import List, Tuple
+from time import sleep
 
-
-point1 = (0, 0)
-point2 = (1, 1)
-point3 = (1, 2)
 
 # Separate x and y coordinates
 # x_coords, y_coords = zip(*points)
 
 Point = Tuple[float, float]
 # points = [(0, 0), (1, 2), (3, 1), (4, 4), (2, 3)]
-
-# Initial data ─ edit or extend this list as you like
-points: List[Point] = [
-    point1,
-    point2,
-    point3,
-]
 
 
 class Plot:
@@ -35,13 +25,13 @@ class Plot:
         self.ax.set_ylabel("Y")
         self.ax.set_title("Dynamic line plot – edit points at runtime")
         self.ax.grid(True)
-        self.plt.pause(0.01)  # ensure canvas refreshes
+        plt.pause(0.01)  # ensure canvas refreshes
 
     def add_point(self, pts: List[Point], x: float, y: float) -> None:
         """Append a new (x, y) tuple to *pts*."""
         pts.append((x, y))
 
-    def update_point(pts: List[Point], index: int, x: float, y: float) -> None:
+    def update_point(self, pts: List[Point], index: int, x: float, y: float) -> None:
         """Replace the point at *index* with a new (x, y) pair."""
         if 0 <= index < len(pts):
             pts[index] = (x, y)
@@ -50,24 +40,34 @@ class Plot:
 
     def update(self, new_pts):
         xs, ys = zip(*new_pts)
-        line.set_data(xs, ys)  # 1. mutate
+        # line.set_data(xs, ys)  # 1. mutate
         self.ax.relim()
         self.ax.autoscale_view()
         self.fig.canvas.draw_idle()  # 2. ask for redraw
         plt.pause(0.01)
 
     def create_plot(self) -> None:
-        self.draw_lines(ax, points)
+        points: List[Point] = [
+            # (0, 0),
+            # (1, 1),
+            # (1, 2),
+        ]
+        self.draw_lines(points)
 
-        # add_point(points, 5, 2)  # add a new point
-        # draw_lines(ax, points)  # refresh plot
-
-        update_point(points, 2, 1, 1)  # move first point
-        draw_lines(ax, points)  # refresh plot
+        self.add_point(points, 5, 2)  # add a new point
+        self.draw_lines(points)  # refresh plot
 
         print("Close the window to end the program.")
+        plt.show(block=True)
+
+        sleep(2)
+
         plt.show(block=False)
+        self.update_point(pts=points, index=2, x=1, y=1)  # move first point
+        self.draw_lines(points)  # refresh plot
 
 
 if __name__ == "__main__":
-    create_plot()
+
+    plot = Plot()
+    plot.create_plot()
